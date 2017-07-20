37666ae683eb6e17df887adf3192785958dbdf21
Abhishek Chatterjee <abhishek.chatterjee@philips.com>
2017-07-20 13:52:30 +0530

    Updated app.py file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json, request
-from flask_mysqldb import MySQL
+from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
